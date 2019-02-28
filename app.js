0d61b7dc385b073d7c08182834dd5c00cac1b0c4
Gowtham <gowthamsm26@gmail.com>
2019-02-28 16:00:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
-var T = new Twitter(config);
+var client = new Twitter(config);
 
 var params = {
-    q: '#CaptainMarvelTrailer',
-    count: 3,
+    q: '#CaptainMarvel',
+    count: 2,
     result_type: 'recent',
     lang: 'en'
 }
 
-T.get('search/tweets', params, function(err, data, response){
+client.get('search/tweets', params, function(err, data, response){
     if(!err){
         for(let i=0; i < data.statuses.length; i++){
             let id =  { id: data.statuses[i].id_str }
-            T.post('favorites/create', id, function(err, response){
+            client.post('favorites/create', id, function(err, response){
                 if(err){
                     console.log(err[0].message);
                 }else{
